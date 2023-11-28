ee87053c70a415baf4283ff8f22fb0c6c76c78ba
DiogoConcei <diogoliveiraspot@gmail.com>
2023-11-28 16:58:29 -0300

    modificações_recentes

--- conversor.js
+++ conversor.js
@@ -1,28 +1,52 @@
-// Pacotes
 const fs = require("fs");
 const path = require("path");
 const pdf2img = require("pdf-img-convert");
 
-// Variáveis
-const paths = [];
+function Ler_Arquivos(Diretorio, Converter_Arquivos) {
+  try {
+    const files = fs.readdirSync(Diretorio);
+    console.log("Arquivos encontrados no diretório:");
 
-// Lista todos os arquivos presentes no
-
-function Ler_Arquivos(Diretorio) {
-  fs.readdir(Diretorio, (err, files) => {
-    if (err) {
-      console.error("Erro ao ler o diretório:", err);
-      return;
-    }
-
-    // Mostra os arquivos no console
-    console.log("Arquivos no diretório:");
+    const paths_files = [];
 
     files.forEach((file) => {
       console.log(file);
-      paths.push(path.resolve(file));
+      paths_files.push(path.resolve(Diretorio, file));
     });
-  });
+
+    // Chama a função de callback  passando os caminhos dos arquivos
+    Converter_Arquivos(null, paths_files);
+  } catch (err) {
+    // Em caso de erro, chama a função de callback  com o erro
+    Converter_Arquivos(err, null);
+  }
 }
 
-Ler_Arquivos('./pdf_test');
+function Converter_Arquivos(err, paths_files) {
+  const temp = "./temp";
+
+  if (err) {
+    console.error("Erro encontrado : ", err);
+    return;
+  }
+
+    if (!fs.existsSync(temp)) {
+      fs.mkdirSync(temp);
+    }
+  
+
+    paths_files.forEach((pdfFilePath) => {
+      pdf2img.convert(pdfFilePath).then((ImgSaida) => {
+        ImgSaida.forEach((image, index) => {
+          const imagePath = path.join(temp, `imagem_${index + 1}.png`);  
+          fs.writeFileSync(imagePath, image);
+        });
+      }).catch((error) => {
+        console.error("Erro durante a conversão : ", error);
+      });
+    });
+  }
+  
+
+// Exemplo de uso
+Ler_Arquivos("./pdf_test", Converter_Arquivos);
